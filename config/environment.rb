76d63a9fcb760d7e793d1c91a472ed244a2f7509
iamkinglana <ngetichenock32@gmail.com>
2023-05-23 00:33:37 +0300

    First Commit

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,18 @@
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
+
+#!/usr/bin/env ruby 
+require_relative '../config/environment'
+require_relative '../lib/garden.rb'
+require_relative '../lib/plant.rb'
+
+lawn = Garden.new(name: 'Front Lawn')
+
+basil = Plant.new(name: 'Basil')
+basil.garden = lawn
+
+cucumber = Plant.new(name: 'Cucumber')
+cucumber.garden = lawn
+
+p lawn.plants
